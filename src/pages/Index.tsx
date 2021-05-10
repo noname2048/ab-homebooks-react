bf7ff1e65270625fa85ef930a88af2f1b0725a35
noname2048 <sungwook.csw@gmail.com>
2021-05-10 21:32:49 +0900

    ant design added

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,8 @@ import { Route } from "react-router-dom";
 
 export default function Root() {
     return (
-        <Route exact path="/"></Route>
+        <>
+            <Route exact path="/"></Route>
+        </>
     )
 }

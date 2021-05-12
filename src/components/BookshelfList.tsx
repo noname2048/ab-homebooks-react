8bf1b2ac41adde56de061682242d98e7d7b9d493
noname2048 <sungwook.csw@gmail.com>
2021-05-12 23:43:08 +0900

    new: bookshelf list
    login added (jwt login need)
    bookshelf list shows username + bookshelfName
    
    none

--- src/components/BookshelfList.tsx
+++ src/components/BookshelfList.tsx
@@ -6,20 +6,29 @@ const BookshelfList = () => {
   const [bookshelfList, setBookshelfList] = useState([]);
 
   useEffect(() => {
-    Axios.get("http://localhost:8000/")
+    // "http://localhost:8000/bookshelves/api/bookshelves/"
+    // "http://localhost:8010/instagram/api/posts/"
+    Axios.get("http://localhost:8000/bookshelves/api/bookshelves/")
       .then((response) => {
+        console.log(response.data);
         setBookshelfList(response.data);
       })
       .catch((error) => {
         console.log(error);
+        console.log("왜안대");
       });
   }, []);
 
   return (
     <div>
-      {bookshelfList.map((bookshelf) => {
+      {/* {bookshelfList.map((bookshelf) => {
         return <div>{bookshelf}</div>;
-      })}
+      })} */}
+      {bookshelfList.map(
+        (bookshelf: { user: { username: string }; name: string }) => (
+          <p key={bookshelf.name}>{bookshelf.user.username}님의 책장</p>
+        )
+      )}
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,9 @@ import "antd/dist/antd.css";
 import { BookOutlined, HomeOutlined } from "@ant-design/icons";
 import { Layout, Menu } from "antd";
 
+import BookshelfList from "components/BookshelfList";
 import React from "react";
+import { Route } from "react-router-dom";
 
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -16,6 +18,7 @@ const Home = () => {
           <h1>Header</h1>
         </Header>
         <Layout>
+          <Route></Route>
           <Sider>
             <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
               <Menu.Item key="1" icon={<BookOutlined />}>
@@ -27,7 +30,9 @@ const Home = () => {
               <Menu.Item key="3">nav 3</Menu.Item>
             </Menu>
           </Sider>
-          <Content>Content</Content>
+          <Content>
+            <BookshelfList />
+          </Content>
         </Layout>
         <Footer>Footer</Footer>
       </Layout>
